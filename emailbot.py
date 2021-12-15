47d193c280b0755aef1a2d36b0ced56f2c2f8b52
chromeosisBS <82548474+chromeosisBS@users.noreply.github.com>
2021-12-15 14:59:38 -0600

    Update emailbot.py

--- emailbot.py
+++ emailbot.py
@@ -6,23 +6,24 @@ import os
 port = 587  # For starttls
 smtp_server = "smtp.gmail.com" # choose smtp server default is google
 # get sender creds
-sender_email = "emailbottesting@gmail.com" # put your email here
-password = getpass("type your password and press enter:")
+def login():
+    sender_email = "emailbottesting@gmail.com" # put your email here
+    password = getpass("type your password and press enter:")
+
 # get reciever email
-receiver_email= input("Enter  receiver email: ")
+def reciever():
+    receiver_email= input("Enter  receiver email: ")
 f=open("message.txt", "r")
 if f.mode == 'r':
 
-	message = f.read()
+    message = f.read()
 context = ssl.create_default_context()
 with smtplib.SMTP(smtp_server, port) as server:
     server.ehlo()  # Can be omitted
     server.starttls(context=context)
     server.ehlo()  # Can be omitted
-print (message)
-while 1:
+def send(): 
+    print (message)
     server.login(sender_email, password)
     server.sendmail(sender_email, receiver_email, message)
-        else:
-            server.login(sender_email, password)
-            server.sendmail(sender_email, receiver_email, message)
+        
